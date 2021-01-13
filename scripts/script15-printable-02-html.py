685c436d8c02288cd6a478a4159abbfc6039014a
Luis Gonzalez Morales <gonzalezmorales@gmail.com>
2021-01-13 04:43:43 -0500

    refine pdf creation

--- scripts/script15-printable-02-html.py
+++ scripts/script15-printable-02-html.py
@@ -11,6 +11,27 @@ import re
 from selenium import webdriver
 from bs4 import BeautifulSoup
 
+
+import pdfkit 
+
+last_url = None
+
+options = {
+  "enable-local-file-access": None,
+  'page-size': 'Letter',
+    'margin-top': '1.25in',
+    'margin-right': '1in',
+    'margin-bottom': '0.75in',
+    'margin-left': '1.25in',
+    'image-dpi' : '850',
+    'footer-font-size': '8',
+    'header-font-size': '8',
+    'header-right': 'World\'s Women 2020',
+    'footer-line': None,
+    'footer-right': 'Statistics Division',
+    'footer-left': 'United Nations Department of Economic and Social Affairs',
+}
+
 graph_count = 1
 
 json_dir = 'printable/json/'
@@ -20,7 +41,7 @@ data_files = os.listdir('printable/json/')
 for item in data_files:
 
 
-    if item != '0d1ff2530f17451bb8437c6ea584282e.json':
+    if item != '022ab89e10d04700901ced2a133eda86.json':
         continue
 
     with open('printable/json/' + item) as json_file:
@@ -65,6 +86,9 @@ for item in data_files:
 
                         if i == 0:
                             title = soup2.find('div', attrs={'class': 'title'}).string
+                            title = str(title).replace('&nbsp;', '')
+                            title = title.replace(u'\u00A0', '')
+
                             with tag('h5'):
                                 text(theme)
                             with tag('h1'):
@@ -72,12 +96,22 @@ for item in data_files:
                                     text(title)
                         else:
                             subtitle = soup1.get_text()
-                            content = str(soup2).replace('⇡','&#8673;').replace(u'\u00a0', ' ').replace(u'\u2013', '&ndash;').replace(u'\u201c', '&#8220;').replace(u'\u201d', '&#8221;')
+                            
+                            subtitle = str(subtitle).replace('&nbsp;', '')
+                            subtitle = subtitle.replace(u'\u00A0', '')
+
+                            content = str(soup2).replace('⇡','&#8673;')
+                            content = content.replace(u'\u00a0', ' ')
+                            content = content.replace(u'\u2013', '&ndash;')
+                            content = content.replace(u'\u201c', '&#8220;')
+                            content = content.replace(u'\u201d', '&#8221;')
+                            content = content.replace(u'\u2019', '&#8217;')
+                            content = content.replace(u'\u2014', '&#8212;')
                             print(content)
                             
 
                             content = re.sub(r'\(<a(.*?)back to text</a>\)', "", content)
-
+                            content = content.replace('(&#8673; back to text)', '')
 
                             with tag('div', klass='page'):
                                 doc.stag('br')
@@ -90,30 +124,36 @@ for item in data_files:
                         
                         if media['type'] == 'webpage':
                             url = media['webpage']['url']
-                            graph_count += 1
-
-                            driver = webdriver.Chrome('C:/Users/LGONZALE20/Downloads/chromedriver.exe')
-                            driver.get(url)
-                            driver.execute_script('document.body.style.zoom = "85%"')
-                            image_file = '../img/image_'+ str(graph_count) +'.png'
-                            screenshot = driver.save_screenshot(image_file)
-                            driver.quit()
-
-
-                            doc.stag('img', src=image_file, width='800')
+                            
+                            if url != last_url:
                                 
+                                graph_count += 1
+
+                                driver = webdriver.Chrome('C:/Users/LGONZALE20/Downloads/chromedriver.exe')
+                                driver.get(url)
+                                driver.execute_script('document.body.style.zoom = "85%"')
+                                screenshot = driver.save_screenshot('printable/img/image_'+ str(graph_count) +'.png')
+                                
+                                image_file = '../img/image_'+ str(graph_count) +'.png'
+                                print(image_file)
+                                driver.quit()
+
+                                doc.stag('img', src=image_file, width='800')
+                            
+                            last_url = url
 
                         if media['type'] == 'image':
                             url = media['image']['url']
                             
-                            if i == 0:
-                                doc.stag('img', src=url, height='600')
-                            if i > 1:
-                                doc.stag('img', src=url, style='height:400px;max-width:500px;width: expression(this.width > 500 ? 500: true);')
+                            if url != last_url:
+                                if i == 0:
+                                    doc.stag('img', src=url, height='600')
+                                if i > 1:
+                                    doc.stag('img', src=url, style='height:400px;max-width:500px;width: expression(this.width > 500 ? 500: true);')
 
+                            last_url = url
 
-
-    result = indent(doc.getvalue())
+    result = doc.getvalue()
     
     filename = 'printable/html/' + item.replace('.json','') + '.html'
 
@@ -124,3 +164,8 @@ for item in data_files:
         sys.stdout = f # Change the standard output to the file we created.
         print(result)
         sys.stdout = original_stdout # Reset the standard output to its original value
+
+    
+
+
+    pdfkit.from_file(filename,  'printable/pdf/' + item.replace('.json','') + '.pdf', options=options) 
